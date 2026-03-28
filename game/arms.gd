0fb28569071c3fd843add9aa9c31fc2f054f10c5
Michael <hirt.michaelelias@gmail.com>
2026-03-28 22:21:44 +0100

    biite

--- game/arms.gd
+++ game/arms.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+@onready var armR: Node2D = $ArmR
+@onready var armL: Node2D = $ArmL
+
 @onready var armR_palm: CharacterBody2D = $ArmR/Palm
 @onready var armL_palm: CharacterBody2D = $ArmL/Palm
+
+@onready var armR_animated_sprite_2d: AnimatedSprite2D = $ArmR/Palm/AnimatedSprite2D
+@onready var armL_animated_sprite_2d: AnimatedSprite2D = $ArmL/Palm/AnimatedSprite2D
 
 var mouse_hover_zoneL: bool
 var mouse_hover_zoneR: bool
@@ -11,6 +17,19 @@ var active_hand_is_L: bool
 
 func _ready() -> void:
 	pass
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if not active_hand_is_L:
+				armR_animated_sprite_2d.animation = "close"
+			else:
+				armL_animated_sprite_2d.animation = "close"
+		else:
+			armR_animated_sprite_2d.animation = "open"
+			armL_animated_sprite_2d.animation = "open"
+
 
 func _physics_process(_delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
@@ -27,7 +46,16 @@ func _physics_process(_delta: float) -> void:
 		if active_hand_is_L:
 			armL_palm.global_position = (mouse_pos - armL_palm.global_position) * 0.4 + armL_palm.global_position
 			armL_palm.move_and_slide()
+			armL.z_index = 1
+			armR.z_index = 0
+
 		else:
 			armR_palm.global_position = (mouse_pos - armR_palm.global_position) * 0.4 + armR_palm.global_position
 			armR_palm.move_and_slide()
+			
+			armR.z_index = 1
+			armL.z_index = 0
+			
+			
+	
 	
--- game/rock.gd
+++ game/rock.gd
@@ -1,14 +1,9 @@
 extends StaticBody2D
 
 func reset_size():
-	$CollisionShape2D.shape.height = 20
-	$CollisionShape2D.shape.radius= 10
+	change_sprite()
 
-func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		if event.is_pressed() and not event.is_echo():
-			$CollisionShape2D.shape.height = 50
-			$CollisionShape2D.shape.radius = 15
-		elif event.is_released():
-			$CollisionShape2D.shape.height = 20
-			$CollisionShape2D.shape.radius = 10
+
+func change_sprite():
+	$Sprite2D.frame = randi_range(0,4)
+	
