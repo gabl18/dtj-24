1933df57e8051d5085e0333de3042e1601f43b52
Michael <hirt.michaelelias@gmail.com>
2026-03-29 16:19:23 +0200

    texturesUI

--- ui.gd
+++ ui.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
