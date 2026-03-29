3a35d10f970483c8eeb528793d36e50d18ccba72
Michael <hirt.michaelelias@gmail.com>
2026-03-29 17:35:44 +0200

    MUSIX

---
+++ music_player.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@onready var sfx: AudioStreamPlayer = %SFX
+@onready var music: AudioStreamPlayer = %Music
+
+const MAIN = preload("uid://ytyoymxutedu")
+const MINI_1 = preload("uid://cmij6ls0pncso")
+const MINI_2 = preload("uid://2b1cm6t2g3pc")
+const MINI_3 = preload("uid://ciiessa0bw1os")
+const MINI_4 = preload("uid://buhvq2tntd7xx")
+
+func get_random_mini_track():
+	return [MINI_1,MINI_2,MINI_3,MINI_4].pick_random()
--- ui.gd
+++ ui.gd
@@ -1,8 +1,31 @@
 extends Control
 
+signal start_game
+
+@onready var music_slider: HSlider = %Music_Slider
+@onready var sfx_slider: HSlider = %SFX_Slider
+
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	var music_db = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))
+	var sfx_db = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX"))
+	music_slider.value = db_to_linear(music_db)
+	sfx_slider.value = db_to_linear(sfx_db)
+	
+	music_slider.value_changed.connect(_on_music_slider_value_changed)
+	sfx_slider.value_changed.connect(_on_sfx_slider_value_changed)
 
 
 func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	start_game.emit()
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	var db = linear_to_db(value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), db)
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	var db = linear_to_db(value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), db)
